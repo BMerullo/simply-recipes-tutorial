0d7e2253035199c868a0309ee5bdf66a76e9da9d
Bob Merullo <bmerullo85@gmail.com>
2022-04-22 14:36:39 -0600

    Started image lesson

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,49 @@
-import React from "react"
-import Layout from "../components/Layout"
+import React from "react";
+import Layout from "../components/Layout";
 
 const Contact = () => {
   return (
     <Layout>
-      <h1>contact page</h1>
+      <main className="page">
+        <section className="contact-page">
+          <article className="contact-info">
+            <h3>want to get in touch?</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
+              natus sunt, magni assumenda autem laudantium obcaecati
+              perspiciatis ratione velit
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
+              natus sunt,
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
+              natus sunt, magni assumenda autem laudantium obcaecati
+              perspiciatis ratione velit explicabo voluptas molestias
+            </p>
+          </article>
+          <article>
+            <form className="form contact-form">
+              <div className="form-row">
+                <label htmlFor="name">your name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="email">your email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className="form-row">
+                <label htmlFor="message">message</label>
+                <textarea name="message" id="message"></textarea>
+                <button className="btn block">submit</button>
+              </div>
+            </form>
+          </article>
+        </section>
+      </main>
     </Layout>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
